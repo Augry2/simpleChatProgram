7b398aa63c81f3ebe24dcd56fcb022948fc9c8f3
august <august.rydnell@gmail.com>
2024-02-09 12:50:52 +0100

    working chat program

--- src/main/java/org/example/Client.java
+++ src/main/java/org/example/Client.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 public class Client {
 
     static String serverAddress = "localhost"; // Assuming the server is running on the same machine
-    static int serverPort = 8080;
+    static int serverPort = 1883;
     static Scanner fetch = new Scanner(System.in);
 
     public static void main(String[] args) {
